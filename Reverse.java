6fa54f28b72b30fe0fa969e6d4f211a4c209656d
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2022-08-28 21:04:21 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,31 @@
+echo "# reverse-a-string" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Molly-12/reverse-a-string.git
+git push -u origin main
+
+
+//recurive solution for reversing a string
+import java.util.*;
+class Solution{
+    public static  String rev(String s){
+        if(s.isEmpty()){                  //checking the base condition of the string is empty or not
+            return s;
+        }
+        else{
+            
+        
+        return rev(s.substring(1))+s.charAt(0);    //recursive call for reversing a string
+        }
+    }
+    public static void main(String[] args){     //main function
+        Scanner sc=new Scanner(System.in);
+        
+        String s=sc.next();            //reading input
+        
+        System.out.println(rev(s));  //calling the recursive function and printing the output
+        
+    }
+}
